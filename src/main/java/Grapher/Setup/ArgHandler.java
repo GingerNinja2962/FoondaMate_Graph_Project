b001aa723c7e331127ea7304d0dc228651ef6fe0
Henry <henryswessels0099@gmail.com>
2022-03-20 17:16:54 +0200

    added docstrings
    added doc strings to the ArgHandler and ther Trim Data classes.

--- src/main/java/Grapher/Setup/ArgHandler.java
+++ src/main/java/Grapher/Setup/ArgHandler.java
@@ -2,9 +2,17 @@ package Grapher.Setup;
 
 import Grapher.Printer.DataManager.LocalDateFormatter;
 
-import java.time.LocalDate;
 import java.util.regex.Pattern;
+import java.time.LocalDate;
 
+/** A simple class that handles the command line Arguments, by checking
+ * the -s, -e, -g and -a arguments and setting the data to the formatted data
+ * following the -s, -e, -g and -a modifiers.
+ *
+ * @author Henry Wessels
+ * @version 1.0
+ * @since 2022-03-20
+ */
 public class ArgHandler {
     private String[] orgArgs;
     private String APIURL = "http://sam-user-activity.eu-west-1.elasticbeanstalk.com/";
@@ -13,6 +21,10 @@ public class ArgHandler {
     private String graph;
 
     //region ====[ Constructors ]====
+    /** the constructor for the ArgHandler class.
+     *
+     * @param args the commands line startup arguments as String[].
+     */
     public ArgHandler(String[] args) {
         this.orgArgs = args;
         splitArgs();
@@ -20,24 +32,48 @@ public class ArgHandler {
     //endregion
 
     //region ====[ Getters ]====
+
+    /** Get the APIs endpoint URL.
+     *
+     * @return String containing the API URL.
+     */
     public String getAPIEndPoint() {
         return APIURL;
     }
 
+    /** Get the end date specified in the command line args.
+     *
+     * @return LocalDate containing the end date specified
+     * in the command line args.
+     */
     public LocalDate getEnd() {
         return end;
     }
 
+    /** Get the start date specified in the command line args.
+     *
+     * @return LocalDate containing the Start date specified
+     * in the command line args.
+     */
     public LocalDate getStart() {
         return start;
     }
 
+    /** Get the graph type.
+     *
+     * @return String containing the graph type (bar, histogram, line).
+     */
     public String getGraph() {
         return graph;
     }
     //endregion
 
     //region ====[ Private ]====
+    /** A function that identifies and sets the
+     * start, end, graph and api values by checking
+     * the args by their specifiers (-s -e -g and -a).
+     * @return Nothing.
+     */
     private void splitArgs() {
         String previousArg = "";
         for (String arg : this.orgArgs) {
@@ -51,6 +87,12 @@ public class ArgHandler {
         }
     }
 
+    /** A function that checks the graphs arg against compatible graphs
+     * and sets up the graph variable to the correct graph.
+     *
+     * @param graph The graph argument specified in the command line args.
+     * @return String containing a compatible graph type.
+     */
     private String checkGraphs(String graph) {
         if (Pattern.compile("^\\s*(?:(?i)bar|histogram|line)\\s*$").matcher(graph).find())
             return graph.replaceAll("\\s*", "");
--- src/main/java/Grapher/Setup/TrimData.java
+++ src/main/java/Grapher/Setup/TrimData.java
@@ -6,11 +6,25 @@ import java.util.TreeMap;
 import java.util.SortedMap;
 import java.time.LocalDate;
 
+/** A simple class that trims the APIData's SortedMap to the dates specified
+ * in the command line launch arguments.
+ *
+ * @author Henry Wessels
+ * @version 1.0
+ * @since 2022-03-20
+ */
 public class TrimData {
     private final LocalDate startDate;
     private final LocalDate endDate;
     private TreeMap<LocalDate, Integer> APIData;
 
+    //region ====[ Constructor ]====
+    /** The TrimData class constructor. The APIData is trimmed and
+     * stored within this class upon instantiation.
+     *
+     * @param args ArgHandler containing the command line launch args.
+     * @param APIData DataManager containing the requested data from the API.
+     */
     public TrimData(ArgHandler args, DataManager APIData) {
         this.APIData = new TreeMap<LocalDate, Integer>(APIData.getSortedMap());
         this.startDate = args.getStart();
@@ -18,11 +32,15 @@ public class TrimData {
         trimStartDate();
         trimEndDate();
     }
+    //endregion
 
+    //region ====[ Getters ]====
     public SortedMap<LocalDate, Integer> getTrimmedAPIData() {
         return APIData;
     }
+    //endregion
 
+    //region ====[ Private ]====
     private void trimStartDate() {
         if (this.startDate == null) return;
         if (!this.APIData.firstKey().isBefore(this.startDate)) return;
@@ -36,4 +54,5 @@ public class TrimData {
 
         this.APIData = new TreeMap<LocalDate, Integer>(this.APIData.subMap(this.APIData.firstKey(), true, this.endDate, true));
     }
+    //endregion
 }
